5150a3554a18181c3795ce7f9f5d0d1d189c1c09
Maksim Tolochko <maksim.tolo@gmail.com>
2020-11-10 02:22:37 +0300

    Add nft support

--- challenge-9/substrate-node-template/runtime/src/lib.rs
+++ challenge-9/substrate-node-template/runtime/src/lib.rs
@@ -41,6 +41,8 @@ pub use frame_support::{
 /// Import the template pallet.
 pub use pallet_template;
 
+pub use orml_nft;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -266,6 +268,13 @@ impl pallet_template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl orml_nft::Trait for Runtime {
+	type ClassId = u64;
+	type TokenId = u64;
+	type ClassData = ();
+	type TokenData = ();
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +292,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		OrmlNFT: orml_nft::{Module, Storage},
 	}
 );
 
